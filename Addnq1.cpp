9dcfd934fbcafe6077c69809c65f980c0cbc4be1
prawat05 <prawat_be24@thapar.edu>
2025-10-17 04:54:08 +0530

    Update Addnq1.cpp

--- Addnq1.cpp
+++ Addnq1.cpp
@@ -1,24 +1,38 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-void generateBinary(int n) {
-    queue<string> q;
-    q.push("1");
+vector<int> nearestSmallerElement(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1);   // default -1 if no smaller element exists
+    stack<int> st;               // will store elements (monotonic stack)
 
-    for (int i = 1; i <= n; i++) {
-        string s = q.front();
-        q.pop();
-        cout << s << " ";
-        q.push(s + "0");
-        q.push(s + "1");
+    for (int i = 0; i < n; i++) {
+        // Pop all elements >= current one
+        while (!st.empty() && st.top() >= arr[i]) {
+            st.pop();
+        }
+
+        // If stack is not empty, top is the nearest smaller
+        if (!st.empty()) {
+            result[i] = st.top();
+        }
+
+        // Push current element for next iterations
+        st.push(arr[i]);
     }
+    return result;
 }
 
 int main() {
-    int n = 2;
-    generateBinary(n);
-    return 0;
-}
+    vector<int> arr = {4, 5, 2, 10, 8};
+    vector<int> ans = nearestSmallerElement(arr);
 
+    cout << "Nearest Smaller Elements: ";
+    for (int i = 0; i < ans.size(); i++) 
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
